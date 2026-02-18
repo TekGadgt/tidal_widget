301d1e9f991edba543c249b26da42500c1d12600
Ryan McGovern <tekgadgt@devportfol.io>
2026-02-17 23:30:01 -0500

    continuing work on build time error

---
+++ src/Config.cs
@@ -0,0 +1,8 @@
+// This file is overwritten at build time by the workflow.
+// Do not edit manually.
+static class Config
+{
+    public const string AppName    = "Tidal Now Playing";
+    public const string AppFilter  = "tidal";
+    public const bool   FilterMode = true;
+}
